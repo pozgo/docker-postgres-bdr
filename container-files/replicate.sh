c153749da02e6e2829179fb2f76b4f8fc1ce583f
Przemyslaw Ozgo <linux@ozgo.info>
2017-02-20 16:20:13 +0000

    removed not needed modules.

--- container-files/replicate.sh
+++ container-files/replicate.sh
@@ -24,8 +24,6 @@ if [ $MODE == 'master' ]; then
   log "User Selected replication method: ${MODE}"
   while true; do if cat /var/lib/postgresql/data/pg_log/postgresql*.log | grep "database system is ready to accept connections"; then break; else sleep 1; fi done
   psql $POSTGRES_DB -U $POSTGRES_USER -p $POSTGRES_PORT -c "CREATE EXTENSION IF NOT EXISTS btree_gist;"
-  psql $POSTGRES_DB -U $POSTGRES_USER -p $POSTGRES_PORT -c "CREATE EXTENSION IF NOT EXISTS pg_trgm;"
-  psql $POSTGRES_DB -U $POSTGRES_USER -p $POSTGRES_PORT -c "CREATE EXTENSION IF NOT EXISTS plpgsql;"
   psql $POSTGRES_DB -U $POSTGRES_USER -p $POSTGRES_PORT -c "CREATE EXTENSION IF NOT EXISTS bdr;"
   psql $POSTGRES_DB -U $POSTGRES_USER -p $POSTGRES_PORT -c "SELECT bdr.bdr_group_create(
     local_node_name := '${HOSTNAME}',
@@ -39,8 +37,6 @@ elif [ $MODE == 'slave' ]; then
   log "User Selected replication method: ${MODE}"
   while true; do if cat /var/lib/postgresql/data/pg_log/postgresql*.log | grep "database system is ready to accept connections"; then break; else sleep 1; fi done
   psql $POSTGRES_DB -U $POSTGRES_USER -p $POSTGRES_PORT -c "CREATE EXTENSION IF NOT EXISTS btree_gist;"
-  psql $POSTGRES_DB -U $POSTGRES_USER -p $POSTGRES_PORT -c "CREATE EXTENSION IF NOT EXISTS pg_trgm;"
-  psql $POSTGRES_DB -U $POSTGRES_USER -p $POSTGRES_PORT -c "CREATE EXTENSION IF NOT EXISTS plpgsql;"
   psql $POSTGRES_DB -U $POSTGRES_USER -p $POSTGRES_PORT -c "CREATE EXTENSION IF NOT EXISTS bdr;"
   psql $POSTGRES_DB -U $POSTGRES_USER -p $POSTGRES_PORT -c "SELECT bdr.bdr_group_join(
     local_node_name := '${HOSTNAME}',
